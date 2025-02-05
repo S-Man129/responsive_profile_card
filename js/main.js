79828bfbee3a2a016a878679d79198289fe901ab
S-Man129 <adebayosulayman129@gmail.com>
2025-02-05 18:08:42 +0100

    Update: update image, index.html, stylesheet

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var currentTimeUTC = document.querySelector(".currentTimeUTC");
+
+  // Get the current UTC time
+  let now = new Date();
+  let utcTime = now.toUTCString(); // Converts to a readable UTC format
+
+  // Update the HTML element
+  currentTimeUTC.innerText = utcTime;
+
+  document.querySelectorAll(".allSocial span").forEach((icon) => {
+    icon.addEventListener("click", () => {
+      let platform = icon.getAttribute("data-platform");
+
+      let urls = {
+        facebook: "https://www.facebook.com/Sulayman.Adebayo.SMan/",
+        twitter: "https://twitter.com",
+        linkedin: "https://www.linkedin.com",
+        instagram: "https://www.instagram.com",
+        github: "https://github.com",
+      };
+      if (urls[platform]) {
+        // window.location.href = urls[platform];
+        window.open(urls[platform], "_blank");
+      }
+    });
+  });
+});
